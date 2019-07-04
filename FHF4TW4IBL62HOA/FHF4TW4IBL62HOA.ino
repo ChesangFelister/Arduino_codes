75a31535b994c7cd3b7c3426ea540b4e89d5ab03
ChesangFelister <47848763+ChesangFelister@users.noreply.github.com>
2019-07-04 18:36:45 +0300

    Add files via upload

---
+++ FHF4TW4IBL62HOA/FHF4TW4IBL62HOA.ino
@@ -0,0 +1,37 @@
+
+
+/
+#include <Stepper.h>
+
+
+int dirB = 13;
+int A = 3;
+int B = 11;
+
+
+Stepper stepper1(200, dirA, dirB);
+
+void setup() {
+  
+  pinMode(A, OUTPUT);
+  pinMode(B, OUTPUT);
+  
+ 
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  
+//   stepper motor speed
+  stepper1.setSpeed(60);
+}
+
+void loop(){
+  
+  stepper1.step(100);
+
+  delay(500);
+  
+ 
+  stepper1.step(-100);
+ 
+  delay(500);
+}
